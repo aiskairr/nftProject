227814a099645566d2c68c012f116682839ef621
Aminhan <aminhansss2222@gmail.com>
2022-11-05 14:15:56 +0600

    adaptive

--- src/pages/NFTinfo/NFTinfo.jsx
+++ src/pages/NFTinfo/NFTinfo.jsx
@@ -2,10 +2,10 @@ import css from './NFTinfo.module.scss';
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { Link } from 'react-router-dom';
+import backBtn from '../../images/back_icon.png';
 
 export default function NFTinfo() {
   const [nftInfo, setNftInfo] = useState([]);
-  console.log(nftInfo);
   const { address, token_id } = useParams();
   useEffect(() => {
     fetch(`https://api.opensea.io/api/v1/asset/${address}/${token_id}`)
@@ -21,21 +21,21 @@ export default function NFTinfo() {
         <div className={css.info__wrapper}>
           <p>Name: {nftInfo.name}</p>
           <div>
-            <span>to buy click here: </span>{' '}
+            <span>To buy click here: </span>{' '}
             <a href={nftInfo.permalink}> permalink</a>
           </div>
           <p>
-            asset contract type: {nftInfo?.asset_contract?.asset_contract_type}
+            Asset contract type: {nftInfo?.asset_contract?.asset_contract_type}
           </p>
-          <p>opensea version: {nftInfo?.asset_contract?.opensea_version}</p>
-          <p>schema name: {nftInfo?.asset_contract?.schema_name}</p>
+          <p>Opensea version: {nftInfo?.asset_contract?.opensea_version}</p>
+          <p>Schema name: {nftInfo?.asset_contract?.schema_name}</p>
           <p>
-            seller fee basis points:{' '}
+            Seller fee basis points:{' '}
             {nftInfo?.asset_contract?.seller_fee_basis_points}
           </p>
-          <p>symbol: {nftInfo?.asset_contract?.symbol}</p>
+          <p>Symbol: {nftInfo?.asset_contract?.symbol}</p>
           <Link className={css.back__link} to="/">
-            Назад <img src="./images/back_icon.png" alt="imagess" />
+            Назад &rarr;
           </Link>
         </div>
       </div>
